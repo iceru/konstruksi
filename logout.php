54f2b36356afde79af6c2101a7c246965a84ff0d
iceru <m.hafiz1825@gmail.com>
2019-07-16 11:47:27 +0700

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php 
+session_start();
+session_destroy(); // hapus session yang tersimpan
+
+header('location:login.php'); // kembali kehome
+exit();
+?>
---
+++ sidebar.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+
+
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+			<h3>CV. BENTENG MANDIRI</h3>
+        </div>
+        <ul class="list-unstyled components">
+            <p>Dashboard Admin</p>
+            <li class="active">
+                <a href="#homeSubmenu" >Data Konstruksi</a>
+                </li>
+                <li>
+                    <a href="#">Keuangan</a>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Page 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Admin Login</a>
+                </li>
+                <li>
+                    <a href="logout.php">Sign Out</a>
+                </li>
+            </ul>
+        </nav>
+		     
+
+
+
+
+</html>
